f00fe8e7ecb0913752941c5354502d340c4b8a6e
victorzambrano <ingenieriainversazambrano@gmail.com>
2020-11-10 16:45:01 -0400

    var

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// Variables por tipo de dato
+const numerica = 10;
+const cadena = "Hola Mundo";
+const booleana = true;
+let sinValor;
+
+// Tipos de datos
+console.log(typeof numerica);
+console.log(typeof cadena);
+console.log(typeof booleana);
+console.log(typeof sinValor);
